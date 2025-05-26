2b37807ce05482e701594ebb56a5409a6833b68d
Constantin Kühne <constantin.kuehne@student.hpi.uni-potsdam.de>
2025-05-26 14:47:06 +0200

    created the comnbined data

---
+++ combine.py
@@ -0,0 +1,40 @@
+# %%
+import pandas as pd
+
+# %%
+sales_data = pd.read_csv("./sales_data_transformed.csv")
+sales_data["year_month"] = pd.to_datetime(sales_data["Periods"], format="%Y-%m")
+sales_data = sales_data[sales_data["year_month"] >= "2019-01"]
+sales_data = sales_data[sales_data["year_month"] <= "2025-03"]
+
+# %%
+precipitation_data = pd.read_csv("./monthly_avg_precipitation.csv")
+precipitation_data["year_month"] = pd.to_datetime(precipitation_data["YEAR_MONTH"], format="%Y-%m")
+precipitation_data = precipitation_data.drop(columns=["YEAR_MONTH"])
+precipitation_data = precipitation_data[precipitation_data["year_month"] >= "2019-01"]
+precipitation_data = precipitation_data[precipitation_data["year_month"] <= "2025-03"]
+
+# %%
+wind_data = pd.read_csv("./monthly_national_avg_wind_speed.csv")
+wind_data["year_month"] = pd.to_datetime(wind_data["year_month"], format="%Y-%m")
+wind_data = wind_data[wind_data["year_month"] >= "2019-01"]
+wind_data = wind_data[wind_data["year_month"] <= "2025-03"]
+
+# %%
+temperature_data = pd.read_csv("./monthly_national_avg_temperature_formatted.csv")
+temperature_data["year_month"] = pd.to_datetime(temperature_data["year_month"], format="%Y-%m")
+temperature_data = temperature_data[temperature_data["year_month"] >= "2019-01"]
+temperature_data = temperature_data[temperature_data["year_month"] <= "2025-03"]
+
+# %%
+combined_data = sales_data.merge(precipitation_data, on="year_month", how="left")
+combined_data = combined_data.merge(wind_data, on="year_month", how="left")
+combined_data = combined_data.merge(temperature_data, on="year_month", how="left")
+combined_data = combined_data.rename(columns={
+    "avg_precipitation": "precipitation",
+    "avg_wind_speed": "wind_speed",
+    "avg_temperature": "temperature"
+})
+
+# %%
+combined_data.to_csv("./combined_data.csv", index=True)
