8b6359bcb7843268b8e943e4a90501a7398e81d0
Thijmen Adriaansen <thijmen.a@hotmail.nl>
2025-06-02 20:48:02 +0200

    add search data to master dataset

--- combine.py
+++ combine.py
@@ -26,10 +26,18 @@ temperature_data["year_month"] = pd.to_datetime(temperature_data["year_month"], 
 temperature_data = temperature_data[temperature_data["year_month"] >= "2019-01"]
 temperature_data = temperature_data[temperature_data["year_month"] <= "2025-03"]
 
+
+# %%
+search_data = pd.read_csv("./average_search_data_per_category.csv")
+search_data["year_month"] = pd.to_datetime(search_data["Date"], format="%Y-%m")
+search_data = search_data[search_data["year_month"] >= "2019-01"]
+search_data = search_data[search_data["year_month"] <= "2025-03"]
+
 # %%
 combined_data = sales_data.merge(precipitation_data, on="year_month", how="left")
 combined_data = combined_data.merge(wind_data, on="year_month", how="left")
 combined_data = combined_data.merge(temperature_data, on="year_month", how="left")
+combined_data = combined_data.merge(search_data, on="year_month", how="left")
 combined_data = combined_data.rename(columns={
     "avg_precipitation": "precipitation",
     "avg_wind_speed": "wind_speed",
